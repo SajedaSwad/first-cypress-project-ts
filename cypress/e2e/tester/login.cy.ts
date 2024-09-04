369a2839aadba0c1b580553eef6ff03f41cc4fa6
sajeda swad <201047@ppu.edu.ps>
2024-09-04 15:47:25 +0300

    Login with Incorrect Password

--- cypress/e2e/tester/login.cy.ts
+++ cypress/e2e/tester/login.cy.ts
@@ -4,9 +4,10 @@ import { LoginPage } from "@selector/login-page";
 describe("login page", () => {
   const loginPage = new LoginPage();
   const userInfo = [
-    { email: "validfuser@gmail.com", password: "validpassword" },
-    { email: "invaliduser@gmail.com", password: "validpassword" }, //not found
-    { email: "invaliduser", password: "validpassword" }, //invalid format
+    { email: "validfuser@gmail.com", password: "validpassword" }, //valid credential
+    { email: "invaliduser@gmail.com", password: "validpassword" }, //not found email
+    { email: "invaliduser", password: "validpassword" }, //invalid format for email
+    { email: "validfuser@gmail.com", password: "123" }, //incorrect password
   ];
 
   beforeEach(() => {
@@ -14,8 +15,8 @@ describe("login page", () => {
   });
 
   it("login with valid credentials", () => {
-    loginPage.enterEmail(userInfo[2].email);
-    loginPage.enterPassword(userInfo[2].password);
+    loginPage.enterEmail(userInfo[3].email);
+    loginPage.enterPassword(userInfo[3].password);
     loginPage.clickLogin();
   });
 });
